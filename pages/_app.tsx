9038f667b256d916578d73b5a001dd9a550615dc
pnayak@stepforwardsolutioncom <pnayak@stepforwardsolution.com>
2022-11-17 19:40:22 +0530

    Initial Project Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from '../src/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+   <>
+     <Head>
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+  </Head>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+   </>
+  )
 }
 
 export default MyApp
---
+++ src/theme.ts
@@ -0,0 +1,30 @@
+import { Inter } from '@next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red, grey } from '@mui/material/colors';
+
+export const inter = Inter({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: grey.A700,
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: inter.style.fontFamily,
+  },
+});
+
+export default theme;
